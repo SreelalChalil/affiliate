478921e3640a59264c678ed925babc8ed2d9c1c3
Sreelal <sreelal.ub@gmail.com>
2018-05-24 10:13:04 +0530

    Header and configration changes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('templates/header');
+		$this->load->view('dashboard');
+		$this->load->view('templates/footer');
 	}
 }
